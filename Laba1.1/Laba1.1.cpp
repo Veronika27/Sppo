8159b448ee4c7b95267a00505aadd0df6f672cd8
Асафьев Александр <asafjew2015@yandex.ru>
2020-05-07 20:12:59 +0300

    Добавьте файлы проекта.

---
+++ Laba1.1/Laba1.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h> 
+
+using namespace std;
+
+float f1(int x)
+{
+    float q = 0;
+    if (x >= 0)
+    {
+        q = pow(-x, 2);
+    }
+    else
+    {
+        q = abs(-x);
+    }
+
+    return q;
+}
+int main()
+{
+    int b;
+    setlocale(LC_ALL, "rus");
+    cout << "Введите диапазон" << endl;
+    cin >> b;
+    for (int i = 0; i < b; i++)
+    {
+        float sum = f1(i);
+        cout << "F1=" << sum << " i=" << i << endl;
+    }
+    system("pause");
+    return 0;
+}
